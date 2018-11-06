e4da04b64c24981853e98bea6b36659099c1460f
Francielly Neves <francielly.neves98@gmail.com>
2018-11-06 13:45:18 -0200

    Update D.cpp

--- 2018-2019 ACM-ICPC Brazil Subregional Programming Contest/D.cpp
+++ 2018-2019 ACM-ICPC Brazil Subregional Programming Contest/D.cpp
@@ -1,19 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-
 int main(){
-	
-ios::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
 	int n;
 	cin>>n; 
 	int cont = 0, a;
-	for (int i = 0; i < n; ++i)
-	{
+	for (int i = 0; i < n; ++i){
 		cin>>a;
-		if(a != 1){
+		if(a != 1)
 			cont++;
-		}
 	}
 	cout<<cont<<endl;
 	return 0;
